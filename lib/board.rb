047934fed7adf47d03b0b365723d65b5ca371990
paulzay <paulotieno2@gmail.com>
2020-07-02 18:12:38 +0300

    display the board when playing

--- lib/board.rb
+++ lib/board.rb
@@ -3,6 +3,7 @@ class Board
 
   def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # update_board = []
   end
 
   def display_board
@@ -12,4 +13,12 @@ class Board
     puts '-----------'
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
+
+  # def updated_board
+  #   puts " #{update_board[0]} | #{update_board[1]} | #{update_board[2]} "
+  #   puts '-----------'
+  #   puts " #{update_board[3]} | #{update_board[4]} | #{update_board[5]} "
+  #   puts '-----------'
+  #   puts " #{update_board[6]} | #{update_board[7]} | #{update_board[8]} "
+  # end
 end
