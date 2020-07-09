e738b8349981c6792ed5940d0967b6efa7c4f141
paulzay <paulotieno2@gmail.com>
2020-07-09 17:52:27 +0300

    add fixes

--- lib/game.rb
+++ lib/game.rb
@@ -19,12 +19,12 @@ class Game
   end
 
   def the_end
-    if (game_board.board[0] == "X" && game_board.board[1] == "X" && game_board.board[2] == "X") || (game_board.board[3] == "X" && game_board.board[4] == "X" && game_board.board[5] == "X") || (game_board.board[6] == "X" && game_board.board[7] == "X" && game_board.board[8] == "X") || (game_board.board[0] == "X" && game_board.board[3] == "X" && game_board.board[6] == "X") || (game_board.board[1] == "X" && game_board.board[4] == "X" && game_board.board[7] == "X") || (game_board.board[2] == "X" && game_board.board[5] == "X" && game_board.board[8] == "X") || (game_board.board[0] == "X" && game_board.board[4] == "X" && game_board.board[8] == "X") || (game_board.board[2] == "X" && game_board.board[4] == "X" && game_board.board[6] == "X")
+    if (game_board.board[0] == 'X' && game_board.board[1] == 'X' && game_board.board[2] == 'X') || (game_board.board[3] == 'X' && game_board.board[4] == 'X' && game_board.board[5] == 'X') || (game_board.board[6] == 'X' && game_board.board[7] == 'X' && game_board.board[8] == 'X') || (game_board.board[0] == 'X' && game_board.board[3] == 'X' && game_board.board[6] == 'X') || (game_board.board[1] == 'X' && game_board.board[4] == 'X' && game_board.board[7] == 'X') || (game_board.board[2] == 'X' && game_board.board[5] == 'X' && game_board.board[8] == 'X') || (game_board.board[0] == 'X' && game_board.board[4] == 'X' && game_board.board[8] == 'X') || (game_board.board[2] == 'X' && game_board.board[4] == 'X' && game_board.board[6] == 'X')
       puts "#{$player_start[0].name} wins!!"
       $not_the_end = false
     end
 
-    if (game_board.board[0] == "O" && game_board.board[1] == "O" && game_board.board[2] == "O") || (game_board.board[3] == "O" && game_board.board[4] == "O" && game_board.board[5] == "O") || (game_board.board[6] == "O" && game_board.board[7] == "O" && game_board.board[8] == "O") || (game_board.board[0] == "O" && game_board.board[3] == "O" && game_board.board[6] == "O") || (game_board.board[1] == "O" && game_board.board[4] == "O" && game_board.board[7] == "O") || (game_board.board[2] == "O" && game_board.board[5] == "O" && game_board.board[8] == "O") || (game_board.board[0] == "O" && game_board.board[4] == "O" && game_board.board[8] == "O") || (game_board.board[2] == "O" && game_board.board[4] == "O" && game_board.board[6] == "O")
+    if (game_board.board[0] == 'O' && game_board.board[1] == 'O' && game_board.board[2] == 'O') || (game_board.board[3] == 'O' && game_board.board[4] == 'O' && game_board.board[5] == 'O') || (game_board.board[6] == 'O' && game_board.board[7] == 'O' && game_board.board[8] == 'O') || (game_board.board[0] == 'O' && game_board.board[3] == 'O' && game_board.board[6] == 'O') || (game_board.board[1] == 'O' && game_board.board[4] == 'O' && game_board.board[7] == 'O') || (game_board.board[2] == 'O' && game_board.board[5] == 'O' && game_board.board[8] == 'O') || (game_board.board[0] == 'O' && game_board.board[4] == 'O' && game_board.board[8] == 'O') || (game_board.board[2] == 'O' && game_board.board[4] == 'O' && game_board.board[6] == 'O')
       puts "#{$player_start[0].name} wins!!"
       $not_the_end = false
     end
@@ -32,19 +32,20 @@ class Game
 
   def a_draw
     return unless @@counter == 9 && $not_the_end
+
     $not_the_end = false
     puts "It's a draw!"
   end
-  
+
   def game_on_p1
     player_choice = gets.chomp.to_i
     print "\n"
     while player_choice <= 0 || player_choice > 9
-      puts "Invalid move!! Choose again"
+      puts 'Invalid move!! Choose again'
       player_choice = gets.chomp.to_i
       print "\n"
     end
-    if game_board.board.map! { |num| num == player_choice ? "#{$player_start[0].first_player_icon}" : num }
+    if game_board.board.map! { |num| num == player_choice ? $player_start[0].first_player_icon.to_s : num }
       game_board.display_board
       print "\n"
       @@counter += 1
@@ -57,11 +58,11 @@ class Game
     player_next_choice = gets.chomp.to_i
     print "\n"
     while player_next_choice <= 0 || player_next_choice > 9
-      puts "Invalid move!! Choose again"
+      puts 'Invalid move!! Choose again'
       player_next_choice = gets.chomp.to_i
       print "\n"
     end
-    if game_board.board.map! { |num| num == player_next_choice ? "#{$player_next[0].second_player_icon.to_s}" : num }
+    if game_board.board.map! { |num| num == player_next_choice ? $player_next[0].second_player_icon.to_s : num }
       game_board.display_board
       print "\n"
       @@counter += 1
