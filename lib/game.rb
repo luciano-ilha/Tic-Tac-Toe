6c6ca0e21eac9e2cde15c8ad1ba8ff592c233817
paulzay <paulotieno2@gmail.com>
2020-07-13 16:59:14 +0300

    add fixes

--- lib/game.rb
+++ lib/game.rb
@@ -14,44 +14,45 @@ class Game
   def all_equal_p1(arr, player_icon)
     if arr[0] == player_icon && arr[1] == player_icon && arr[2] == player_icon
       puts "#{player1.name} wins!"
-      return @not_the_end = false
+      @not_the_end = false
     end
   end
 
   def all_equal_p2(arr, player_icon)
     if arr[0] == player_icon && arr[1] == player_icon && arr[2] == player_icon
       puts "#{player2.name} wins!"
-      return @not_the_end = false
+      @not_the_end = false
     end
   end
 
   def check_win
     all_equal_p1(game_board.board[0..2], player1.first_player_icon) ||
-    all_equal_p1(game_board.board[3..5], player1.first_player_icon) ||
-    all_equal_p1(game_board.board[6..8], player1.first_player_icon) ||
-    all_equal_p1([game_board.board[0], game_board.board[3], game_board.board[6]], player1.first_player_icon) ||
-    all_equal_p1([game_board.board[1], game_board.board[4], game_board.board[7]], player1.first_player_icon) ||
-    all_equal_p1([game_board.board[2], game_board.board[5], game_board.board[8]], player1.first_player_icon) ||
-    all_equal_p1([game_board.board[0], game_board.board[4], game_board.board[8]], player1.first_player_icon) ||
-    all_equal_p1([game_board.board[2], game_board.board[4], game_board.board[6]], player1.first_player_icon) ||
-    all_equal_p2(game_board.board[0..2], player2.second_player_icon) ||
-    all_equal_p2(game_board.board[3..5], player2.second_player_icon) ||
-    all_equal_p2(game_board.board[6..8], player2.second_player_icon) ||
-    all_equal_p2([game_board.board[0], game_board.board[3], game_board.board[6]], player2.second_player_icon) ||
-    all_equal_p2([game_board.board[1], game_board.board[4], game_board.board[7]], player2.second_player_icon) ||
-    all_equal_p2([game_board.board[2], game_board.board[5], game_board.board[8]], player2.second_player_icon) ||
-    all_equal_p2([game_board.board[0], game_board.board[4], game_board.board[8]], player2.second_player_icon) ||
-    all_equal_p2([game_board.board[2], game_board.board[4], game_board.board[6]], player2.second_player_icon)
+      all_equal_p1(game_board.board[3..5], player1.first_player_icon) ||
+      all_equal_p1(game_board.board[6..8], player1.first_player_icon) ||
+      all_equal_p1([game_board.board[0], game_board.board[3], game_board.board[6]], player1.first_player_icon) ||
+      all_equal_p1([game_board.board[1], game_board.board[4], game_board.board[7]], player1.first_player_icon) ||
+      all_equal_p1([game_board.board[2], game_board.board[5], game_board.board[8]], player1.first_player_icon) ||
+      all_equal_p1([game_board.board[0], game_board.board[4], game_board.board[8]], player1.first_player_icon) ||
+      all_equal_p1([game_board.board[2], game_board.board[4], game_board.board[6]], player1.first_player_icon) ||
+      all_equal_p2(game_board.board[0..2], player2.second_player_icon) ||
+      all_equal_p2(game_board.board[3..5], player2.second_player_icon) ||
+      all_equal_p2(game_board.board[6..8], player2.second_player_icon) ||
+      all_equal_p2([game_board.board[0], game_board.board[3], game_board.board[6]], player2.second_player_icon) ||
+      all_equal_p2([game_board.board[1], game_board.board[4], game_board.board[7]], player2.second_player_icon) ||
+      all_equal_p2([game_board.board[2], game_board.board[5], game_board.board[8]], player2.second_player_icon) ||
+      all_equal_p2([game_board.board[0], game_board.board[4], game_board.board[8]], player2.second_player_icon) ||
+      all_equal_p2([game_board.board[2], game_board.board[4], game_board.board[6]], player2.second_player_icon)
   end
 
   def a_draw
     return unless @@counter == 9 && @not_the_end
+
     @not_the_end = false
     puts "It's a draw!"
   end
 
   def game_on_p1
-    if game_board.board.map! { |num| num == player_choice ? "#{player1.first_player_icon}" : num }
+    if game_board.board.map! { |num| num == player_choice ? player1.first_player_icon.to_s : num }
       puts game_board.display_board
       print "\n"
       @@counter += 1
@@ -61,7 +62,7 @@ class Game
   end
 
   def game_on_p2
-    if game_board.board.map! { |num| num == player_choice ? "#{player2.second_player_icon}" : num }
+    if game_board.board.map! { |num| num == player_choice ? player2.second_player_icon.to_s : num }
       puts game_board.display_board
       print "\n"
       @@counter += 1
